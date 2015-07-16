909c3167a9f613a862c927cc6c19465ab97cef8d
Alberto Juan Sebastian <aj.sebastian@alumnos.upm.es>
2015-07-16 18:41:23 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,17 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer',   quizController.answer);
 
 //en branch creditos
 router.get('/author', function(req, res) {
 	res.render('author');                   //creo que así está bien, sino meterle parámetro
 });
+
+//a partir de la dB
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
