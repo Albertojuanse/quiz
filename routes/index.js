dab36501f1cf7ab32577a24d0a7a4243a7a6fd41
Alberto Juan Sebastian <aj.sebastian@alumnos.upm.es>
2015-07-16 18:42:50 +0200

    Añadido autoload

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.get('/', function(req, res) {
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer',   quizController.answer);
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load); //autoload :quizId
+
 //en branch creditos
 router.get('/author', function(req, res) {
 	res.render('author');                   //creo que así está bien, sino meterle parámetro
